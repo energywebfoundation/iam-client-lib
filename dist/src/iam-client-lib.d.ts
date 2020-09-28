4a3a72c2b9f8d3dd547bd9643cd998d3c4ad5751
Daniel Wojno <daniel.wojno@tsh.io>
2020-09-28 17:47:25 +0200

    [MYEN-133] remove prebuild package

--- dist/src/iam-client-lib.d.ts
+++
@@ -1,6 +0,0 @@
-import { IAM } from "./iam";
-import { EnrolmentFormData } from "./models/enrolment-form-data";
-import { DIDAttribute, Encoding, Algorithms, PubKeyType } from "@ew-did-registry/did-resolver-interface";
-export { IAM };
-export { EnrolmentFormData };
-export { DIDAttribute, Encoding, Algorithms, PubKeyType };
--- dist/src/utils/ENS_hash.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function decodeLabelhash(hash: string): string;
-export declare function isEncodedLabelhash(hash: string): boolean;
-export declare function namehash(inputName: string): string;
-export declare function labelhash(unnormalizedLabelOrLabelhash: string): string;
