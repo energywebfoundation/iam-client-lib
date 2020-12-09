e5ab706dcd3947bedf5bcb9f3930209c106412f2
John Henderson <john.henderson@energyweb.org>
2020-12-08 21:10:52 -0800

    MYEN-409 Improvement of React client example

--- examples/react-dapp/src/App.tsx
+++ examples/react-dapp/src/App.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import './App.css'
+import './Login.css'
 import { IAM, CacheServerClient } from 'iam-client-lib'
 import axios from 'axios'
-import Button from '@material-ui/core/Button'
-import { Typography } from '@material-ui/core'
+import logo from './assets/logo.svg'
+import metamaskLogo from './assets/metamask-logo.svg'
+import walletconnectIcon from './assets/wallet-connect-icon.svg'
+import Spinner from './components/Spinner'
 
 const cacheClient = new CacheServerClient({ url: 'https://volta-iam-cacheserver.energyweb.org/' })
 
@@ -18,52 +21,129 @@ type Role = {
   namespace: string
 }
 
-type User = {
-  did: string
-  verifiedRoles: Role[]
-}
-
 function App() {
   const [roles, setRoles] = useState<Role[]>([])
+  const [did, setDID] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<Boolean>(false)
 
-  const handleLogin = async () => {
-    const { did } = await iam.initializeConnection({useMetamaskExtension: false})
-    if (!did) {
-      console.log("unable to retrieve DID")
-      return
-    }
-    const signer = iam.getSigner()
-    const latestBlock = await signer?.provider?.getBlockNumber()
+  const verifyIdentity = async function () {
+    const signer = iam.getSigner();
+    const latestBlock = await signer?.provider?.getBlockNumber();
     const claim = await iam.createPublicClaim({
-      data: { blockNumber: latestBlock },
-    })
-    const { data } = await axios.post<{ token: string }>('/login', {
-      claim,
-    })
+      data: { blockNumber: latestBlock }
+    });
+    const { data } = await axios.post<{ token: string }>(
+      "https://did-auth-demo.energyweb.org/login",
+      {
+        claim
+      }
+    );
     const config = {
-      headers: { Authorization: `Bearer ${data.token}` },
-    }
-    const { data: roles } = await axios.get<Role[]>('/roles', config)
-    const { data: user } = await axios.get<User>('/user', config)
+      headers: { Authorization: `Bearer ${data.token}` }
+    };
+    const { data: roles } = await axios.get<Role[]>(
+      "https://did-auth-demo.energyweb.org/roles",
+      config
+    );
+    setRoles(roles);
+  }
 
-    setRoles(roles)
+  const login = async function (initOptions?: {
+    useMetamaskExtension: boolean;
+  }) {
+    setIsLoading(true);
+    const { did } = await iam.initializeConnection(initOptions);
+    if (did) {
+      setDID(did)
+      try {
+        await verifyIdentity();
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    setIsLoading(false);
+  }
+
+  const loginWithWalletConnect = async function () {
+    setIsLoading(true);
+    const { did } = await iam.initializeConnection();
+    if (did) {
+      setDID(did)
+      try {
+        await verifyIdentity();
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    setIsLoading(false);
+  }
+
+  const logout = function () {
+    setDID('');
+  }
+
+  let loginJsx
+  if (isLoading) {
+    loginJsx =
+     (
+      <div>
+        <Spinner />
+        <span>
+          Loading... (Please sign messages using your connected wallet)
+        </span>
+      </div>
+    )
+  }
+  else if (did) {
+    loginJsx = (
+      <div>
+        <p>Hello user!</p>
+        <p>
+          Your decentralised identifier: <br/> 
+          <strong>{did}</strong>
+        </p>
+        {roles && roles.length > 0 ? (
+          <div className="rolesContainer">
+            <p>These are your validated roles:</p>
+            {roles.map(({ name, namespace }) => (
+              <p key={namespace}><strong>{`${name}`}</strong>{` at ${namespace}`}</p>
+            ))}
+          </div>
+        )
+          : (
+            <div>
+              You do not have any issued role at the moment, please login into switchboard and search for
+              apps, orgs to enrol.
+            </div>
+          )}
+        <div className="logoutContainer">
+          <button onClick={logout} className="button">
+            <span>Logout</span>
+          </button>
+        </div >
+      </div >
+    )
+  }
+  else {
+    loginJsx = (
+      <div className="container">
+        <button className="button" onClick={async () => await loginWithWalletConnect()}>
+          <img alt="walletconnect logo" className="walletconnect" src={walletconnectIcon} />
+          <span>Login with Wallet Connect</span>
+        </button>
+        <button className="button" onClick={async () => await login({ useMetamaskExtension: true })}>
+          <img alt="metamask logo" className="metamask" src={metamaskLogo} />
+          <span>Login with Metamask</span>
+        </button>
+      </div>
+    )
   }
   return (
     <div className="App">
-      <header className="App-header">
-        {roles && roles.length > 0 ? (
-          <>
-            <Typography>User roles:</Typography>
-            {roles.map(({ name, namespace }) => (
-              <p key={namespace}>{`${name} at ${namespace}`}</p>
-            ))}
-          </>
-        ) : null}
-        <Button variant="contained" color="primary" onClick={handleLogin}>
-          login
-        </Button>
-      </header>
-    </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>IAM showcase app</h2>
+        {loginJsx}
+    </div >
   )
 }
 
---
+++ examples/react-dapp/src/components/Spinner.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Spinner.css'
+
+function Spinner() {
+    return <div className='loader' />
+}
+
+export default Spinner
