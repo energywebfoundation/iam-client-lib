06d92c9d759b772fae5169399d620941c81beae0
John Henderson <john.henderson@energyweb.org>
2020-12-08 19:35:44 -0800

    Additional comments to explain changes to bili config

--- bili.config.ts
+++ bili.config.ts
@@ -10,8 +10,12 @@ const config: Config = {
   },
   extendConfig: (config) => ({
     ...config,
+    // The default externals are the package.json dependencies and can only bundle node_modules that are not a dependency.
+    // So need to clear out all externals, to bundle nats.ws package.
     externals: []
   }),
+  // nats.ws is bundled because using standard import/require does not work for cjs format. 
+  // nat.ws only provides a .mjs file which cannot be required.
   bundleNodeModules: ["nats.ws"],
   input: 'src/iam-client-lib.ts',
   output: {
