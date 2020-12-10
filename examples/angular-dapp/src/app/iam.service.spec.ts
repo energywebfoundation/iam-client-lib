e119d72f200d83abb713fce08a575f0495ead7a2
John Henderson <john.henderson@energyweb.org>
2020-12-09 17:29:50 -0800

    [MYEN-410] Angular example implementation

---
+++ examples/angular-dapp/src/app/iam.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { IamService } from './iam.service';
+
+describe('IamService', () => {
+  let service: IamService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(IamService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
