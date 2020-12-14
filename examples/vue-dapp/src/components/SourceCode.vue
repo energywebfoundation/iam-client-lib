2784e0998bebf8c1f8a8d6d7a32a43c08f9b49be
John Henderson <jrhender@users.noreply.github.com>
2020-12-14 03:01:06 -0700

    MYEN 474 GitHub logo with link and MYEN 475 Examples deployment (#59)
    * MYEN-474 github logo with link to readme in examples
    * Made React example background white
    * [MYEN-475] Enabled examples to be served from sub paths

---
+++ examples/vue-dapp/src/components/SourceCode.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="source-code">
+    <p>Source code:</p>
+    <a
+      href="https://github.com/energywebfoundation/iam-client-lib/tree/develop/examples/vue-dapp"
+    >
+      <img src="../assets/GitHub-Mark-32px.png" />
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "SourceCode"
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.source-code {
+  display: flex;
+  position: fixed;
+  right: 0;
+  top: 0;
+
+  img {
+    margin: 10px;
+  }
+}
+</style>
