4c3779c13d1060a9bec94c35aa412532dc32670e
John Henderson <john.henderson@energyweb.org>
2021-01-04 09:13:00 -0800

    Checking global scope for isBrowser

--- src/utils/isBrowser.ts
+++ src/utils/isBrowser.ts
@@ -1,8 +1,8 @@
-export const isBrowser = (processObj: processProps = process) =>
-  typeof processObj === "undefined" || processObj.release?.name.search(/node|io.js/) === -1;
-
-interface processProps {
-    release?: {
-        name: string
-    }
-}
+/**
+ * isBrowser creates a new function using a new Function() constructor.
+ * This is so the execution scope of this is binded to the global scope.
+ * We can compare the global scope directly to the expected value of "window".
+ * An alternative approach of checking a javascript variable (e.g. window or process)
+ * is fallible because javascript variables can be redefined by inner scopes.
+ */
+export const isBrowser = new Function("try {return this===window;} catch(e) {return false;}");
--- test/utils/isBrowser.test.ts
+++ test/utils/isBrowser.test.ts
@@ -1,33 +1,8 @@
-import { isBrowser } from '../../src/utils/isBrowser'
+import { isBrowser } from '../../src/utils/isBrowser';
 
 describe("isBrowser Test", () => {
-    it('returns false if process release name is node', () => {
-        const mockProcess = {
-            release: {
-                name: 'node'
-            }
-        };
-        const result = isBrowser(mockProcess);
+    it('returns false if running in node', () => {
+        const result = isBrowser();
         expect(result).toBeFalsy();
-    });
-
-    it('returns false if process release name is io.js', () => {
-        const mockProcess = {
-            release: {
-                name: 'io.js'
-            }
-        };
-        const result = isBrowser(mockProcess);
-        expect(result).toBeFalsy();
-    });
-
-    it('returns true if process release name is not node', () => {
-        const mockProcess = {
-            release: {
-                name: 'notNode'
-            }
-        };
-        const result = isBrowser(mockProcess);
-        expect(result).toBeTruthy();
     });
 });
