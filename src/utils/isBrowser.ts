f81c3e98d516a5c8fb7c27a7df8c9a987405aa55
John Henderson <jrhender@users.noreply.github.com>
2021-01-05 12:45:33 -0700

    Revert "Fixing isBrowser logic and adding tests"

--- src/utils/isBrowser.ts
+++ src/utils/isBrowser.ts
@@ -1,8 +1,2 @@
-/**
- * Though the process variable can be redefined, this approach is suitable for now.
- * 
- * Another option is to use new Function() constructor so the execution scope of this is binded to the global scope.
- * However, this fails if Content-Security-Policy (CSP) script-src does not allow 'unsafe-eval'.
- */
 export const isBrowser = () =>
-  typeof process === "undefined" || process.release?.name.search(/node|io.js/) === -1;
+  typeof process !== "undefined" && process.release?.name.search(/node|io.js/) !== -1;
