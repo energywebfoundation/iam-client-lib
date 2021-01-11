2d5d0ca7607be0bf3c1d9e1a7749a43401a0f1e2
John Henderson <john.henderson@energyweb.org>
2021-01-11 10:18:07 -0800

    isBrowser always true as not relevant anymore

--- src/utils/isBrowser.ts
+++ src/utils/isBrowser.ts
@@ -1,2 +1,4 @@
-export const isBrowser = () =>
-  typeof process !== "undefined" && process.release?.name.search(/node|io.js/) !== -1;
+// We were previously checking if were inBrowser because there was no way to setup Signer in node.
+// However, we can now set the signer by passing in the privateKey and so this check isn't needed.
+// TODO: Remove references to this function. One thing to check is that NATS works in node.
+export const isBrowser = () => true;
