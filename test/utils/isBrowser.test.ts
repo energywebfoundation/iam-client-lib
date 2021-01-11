3c9562e199fdd5de3185bda476debda9ad3779ad
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2021-01-11 14:12:08 +0200

    fix: correct isBrowser test label

--- test/utils/isBrowser.test.ts
+++ test/utils/isBrowser.test.ts
@@ -1,7 +1,7 @@
 import { isBrowser } from '../../src/utils/isBrowser';
 
 describe("isBrowser Test", () => {
-  it('returns false if running in node', () => {
+  it('returns true if running in browser', () => {
     const result = isBrowser();
     expect(result).toBe(true);
   });
