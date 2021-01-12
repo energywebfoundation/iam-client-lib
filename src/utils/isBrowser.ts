f89f9784855395cf5fadfec1c2ef2a6ccbdee85b
John Henderson <john.henderson@energyweb.org>
2021-01-12 11:51:47 -0800

    Fixing check so that it works in Switchboard where process.release is set

--- src/utils/isBrowser.ts
+++ src/utils/isBrowser.ts
@@ -1,4 +1,2 @@
-// We were previously checking if were inBrowser because there was no way to setup Signer in node.
-// However, we can now set the signer by passing in the privateKey and so this check isn't needed.
-// TODO: Remove references to this function. One thing to check is that NATS works in node.
-export const isBrowser = () => true;
+export const isBrowser = () =>
+  typeof process?.release?.name === "undefined" || process.release.name.search(/node|io.js/) === -1;
