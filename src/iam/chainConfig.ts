2edbe6e875b498ff2244f0fa7b638da108bf3d37
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2021-05-31 11:55:43 +0300

    fix: update asset manager

--- src/iam/chainConfig.ts
+++ src/iam/chainConfig.ts
@@ -25,7 +25,7 @@ export const chainConfigs: Record<number, ChainConfig> = {
     rpcUrl: "https://volta-rpc-vkn5r5zx4ke71f9hcu0c.energyweb.org/",
     ensRegistryAddress: "0xd7CeF70Ba7efc2035256d828d5287e2D285CD1ac",
     ensResolverAddress: "0x0a97e07c4Df22e2e31872F20C5BE191D5EFc4680",
-    assetManagerAddress: "0x262b36981743E7D91d4eB5b3F5ecaEA9d3A638ca",
+    assetManagerAddress: "0xE258fA7D1cc8964D0dEB7204Df947bCa42b2c940",
     didContractAddress: VoltaAddress1056
   }
 };
