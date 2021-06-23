81e526926f904fc4ad6e53fb954d74f725205b08
Ahmed Ibrahim <ahmedolaibrahim@gmail.com>
2021-06-23 08:58:34 +0100

    feedback: update test description

--- test/utilsTests/did.testSuit.ts
+++ test/utilsTests/did.testSuit.ts
@@ -6,13 +6,13 @@ export function didTests() {
     expect(supportedDIDMethods()).toContain(Methods.Erc1056);
   });
 
-  it("should remove DID from address if present", async () => {
+  it("Should extract address from DID", async () => {
     const didAddress = 'did:ethr:0x395569900f9e60819fd5521a9aC044a1B2a849DC8'
     const address = parseDID(didAddress);
     expect(address).toStrictEqual(didAddress.split(':')[2]);
   });
 
-  it("should not attempt to remove did from address if it is not present", async () => {
+  it("Should not extract address from DID if it is not present", async () => {
     const didAddress = '0x395569900f9e60819fd5521a9aC044a1B2a849DC8'
     const address = parseDID(didAddress);
     expect(address).toStrictEqual(didAddress);
