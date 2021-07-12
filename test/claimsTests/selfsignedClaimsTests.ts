64b4b316f94320d320c65d5ee2b9c293ae3f5499
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2021-07-12 21:55:08 +0300

    chore: change create iam in tests

--- test/claimsTests/selfsignedClaimsTests.ts
+++ test/claimsTests/selfsignedClaimsTests.ts
@@ -1,21 +1,37 @@
-import { iam } from "../iam.test";
+import { IAM } from "../../src/iam";
+import { rootOwner } from "../iam.test";
+import { rpcUrl } from "../setup_contracts";
 
 export const selfsignedClaimsTests = function () {
+  let rootOwnerIam: IAM;
+
   const namespace = "daniel.iam.ewc";
 
+  beforeAll(async () => {
+    rootOwnerIam = new IAM({
+      rpcUrl,
+      privateKey: rootOwner.privateKey
+    });
+    await rootOwnerIam.initializeConnection({
+      reinitializeMetamask: false,
+      initCacheServer: false,
+      initDID: true
+    });
+  });
+
   test("ens claim can be created", async () => {
-    await iam.createSelfSignedClaim({
+    await rootOwnerIam.createSelfSignedClaim({
       data: { claimType: namespace, claimTypeVersion: 1 }
     });
-    const { service = [] } = await iam.getDidDocument();
+    const { service = [] } = await rootOwnerIam.getDidDocument();
     expect(service.find(({ claimType }) => claimType === namespace)).toBeTruthy();
   });
   test("ens claim should by updated", async () => {
-    const { service = [] } = await iam.getDidDocument();
+    const { service = [] } = await rootOwnerIam.getDidDocument();
     const { claimType, claimTypeVersion } =
       service.find(({ claimType }) => claimType === namespace) || {};
-    await iam.createSelfSignedClaim({ data: { claimType, claimTypeVersion } });
-    const { service: newServices = [] } = await iam.getDidDocument();
+    await rootOwnerIam.createSelfSignedClaim({ data: { claimType, claimTypeVersion } });
+    const { service: newServices = [] } = await rootOwnerIam.getDidDocument();
     expect(
       newServices.find(
         claim => claim.claimType === claimType && claim.claimTypeVersion === claimTypeVersion
@@ -24,12 +40,12 @@ export const selfsignedClaimsTests = function () {
     expect(newServices.length).toBe(1);
   });
   test("ens claim should be created when claimType do not match", async () => {
-    const { service = [] } = await iam.getDidDocument();
+    const { service = [] } = await rootOwnerIam.getDidDocument();
     const { claimTypeVersion } = service.find(({ claimType }) => claimType === namespace) || {};
-    await iam.createSelfSignedClaim({
+    await rootOwnerIam.createSelfSignedClaim({
       data: { claimType: "edvin.iam.ewc", claimTypeVersion }
     });
-    const { service: newServices = [] } = await iam.getDidDocument();
+    const { service: newServices = [] } = await rootOwnerIam.getDidDocument();
     expect(
       newServices.find(
         claim => claim.claimType === "edvin.iam.ewc" && claim.claimTypeVersion === claimTypeVersion
@@ -38,35 +54,35 @@ export const selfsignedClaimsTests = function () {
     expect(newServices.length).toBe(2);
   });
   test("ens claim should be created when claimTypeVersion do not match", async () => {
-    await iam.createSelfSignedClaim({
+    await rootOwnerIam.createSelfSignedClaim({
       data: { claimType: namespace, claimTypeVersion: 2 }
     });
-    const { service: newServices = [] } = await iam.getDidDocument();
+    const { service: newServices = [] } = await rootOwnerIam.getDidDocument();
     expect(
       newServices.find(claim => claim.claimType === namespace && claim.claimTypeVersion === 2)
     ).toBeTruthy();
     expect(newServices.length).toBe(3);
   });
   test("ens claim should not be created", async () => {
-    await iam.createSelfSignedClaim({
+    await rootOwnerIam.createSelfSignedClaim({
       data: { claimType: namespace, claimTypeVersion: 2 }
     });
-    const { service: newServices = [] } = await iam.getDidDocument();
+    const { service: newServices = [] } = await rootOwnerIam.getDidDocument();
     expect(newServices.length).toBe(3);
   });
 
   test("profile claim should be created", async () => {
-    await iam.createSelfSignedClaim({
+    await rootOwnerIam.createSelfSignedClaim({
       data: { profile: { name: "Edwin" } }
     });
-    const { service = [] } = await iam.getDidDocument();
+    const { service = [] } = await rootOwnerIam.getDidDocument();
     expect(service.find(claim => claim.profile && claim.profile.name === "Edwin")).toBeTruthy();
   });
   test("profile claim should be updated", async () => {
-    const { service = [] } = await iam.getDidDocument();
+    const { service = [] } = await rootOwnerIam.getDidDocument();
     const { id } = service.find(({ profile }) => Boolean(profile)) || {};
-    await iam.createSelfSignedClaim({ data: { profile: { name: "Dan" } } });
-    const { service: updatedService = [] } = await iam.getDidDocument();
+    await rootOwnerIam.createSelfSignedClaim({ data: { profile: { name: "Dan" } } });
+    const { service: updatedService = [] } = await rootOwnerIam.getDidDocument();
     const { profile } = updatedService.find(({ id: claimId }) => id === claimId) || {};
     expect(profile?.name).toBe("Dan");
     expect(updatedService.length).toBe(4);
