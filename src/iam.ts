d2b5ac86ce1c6ceb20e39d3dd56ec8ab8dc1b9ba
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2021-07-20 10:58:20 +0300

    fix: rm double develop fetch

--- src/iam.ts
+++ src/iam.ts
@@ -486,8 +486,6 @@ export class IAM extends IAMBase {
    *
    */
   async issuePublicClaim({ token, publicClaim }: { token?: string, publicClaim?: IPublicClaim }) {
-    console.log(publicClaim, token);
-    
     if (this._issuerClaims) {
       if (publicClaim) {
         return this._issuerClaims.issuePublicClaim(publicClaim);
