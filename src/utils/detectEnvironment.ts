8c8edca1ed9ea4b696cd81a3b1ca72e3e6358eee
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2021-08-26 10:50:23 +0300

    fix: catch env detect error

--- src/utils/detectEnvironment.ts
+++ src/utils/detectEnvironment.ts
@@ -6,6 +6,6 @@ export enum ExecutionEnvironment {
 export const detectExecutionEnvironment = () =>
     (isNode() && ExecutionEnvironment.NODE) || (isBrowser() && ExecutionEnvironment.BROWSER);
 
-const isNode = () => new Function("return this===global");
+const isNode = new Function("try { return this === global } catch(e) { return false; }");
 
-const isBrowser = () => new Function("return this===window");
+const isBrowser = new Function("try { return this === window } catch(e) { return false; }");
