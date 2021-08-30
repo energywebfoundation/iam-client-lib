e568aee15692041e4b3c3e743c3759ee09aef3a7
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2021-08-30 18:02:39 +0300

    add did registry service

--- src/modules/didRegistry/didRegistry.service.ts
+++ src/modules/didRegistry/didRegistry.service.ts
@@ -1,18 +1,37 @@
 import { Injectable } from "@nestjs/common";
-import { IdentityOwner } from "@ew-did-registry/did-ethr-resolver";
+import { Wallet, providers } from "ethers";
+import { EwJsonRpcSigner, EwPrivateKeySigner, IdentityOwner } from "@ew-did-registry/did-ethr-resolver";
+import { ProviderTypes } from "@ew-did-registry/did-resolver-interface";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 import { SignerService } from "../signer/signer.service";
-import { Owner } from "./Owner";
+import { ERROR_MESSAGES } from "../../errors";
 
+const { JsonRpcProvider, Web3Provider } = providers;
 @Injectable()
 export class DidRegistry {
     private _identityOwner: IdentityOwner;
     constructor(private _signerService: SignerService) {}
 
     async connectToDIDRegistry() {
-        this._identityOwner = new IdentityOwner.fromJsonRpcSigner(
-          this._signerService.signer.
-        );
-        await this.setDocument();
-        this.setClaims();
+        const signer = this._signerService.signer;
+        const provider = signer.provider;
+        const publicKey = this._signerService.publicKey;
+        if (signer instanceof Wallet && provider instanceof JsonRpcProvider) {
+            this._identityOwner = IdentityOwner.fromPrivateKeySigner(
+                new EwPrivateKeySigner(signer.privateKey, {
+                    type: ProviderTypes.HTTP,
+                    uriOrInfo: provider.connection.url,
+                }),
+            );
+        } else if (provider instanceof WalletConnectProvider) {
+            this._identityOwner = IdentityOwner.fromJsonRpcSigner(new EwJsonRpcSigner(provider), publicKey);
+        } else if (provider instanceof Web3Provider) {
+            this._identityOwner = IdentityOwner.fromJsonRpcSigner(
+                new EwJsonRpcSigner(provider.jsonRpcFetchFunc),
+                publicKey,
+            );
+        } else {
+            throw new Error(ERROR_MESSAGES.PROVIDER_NOT_INITIALIZED);
+        }
     }
 }
