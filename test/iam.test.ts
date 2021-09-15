24607ffe9fa1969fa8d6ca3087f886122a25efc9
knzeng-e <kevin.nzeng@gmail.com>
2021-09-15 21:48:25 +0200

    cleanup: removed unecessary comments

--- test/iam.test.ts
+++ test/iam.test.ts
@@ -49,14 +49,6 @@ export const createIam = async (privateKey: string, { createDocument = false, in
     return iam;
 };
 
-// const identityTests = () => {
-//     test("Can create identityToken", async () => {
-//         const identityToken = await rootOwnerIam.createIdentityProof();
-//         console.log("Identity created ==> ", identityToken);
-//         expect(identityToken).toBeDefined;
-//     })
-// }
-
 beforeAll(async () => {
     // sometimes transaction is taking more then default 5000 ms jest timeout
     jest.setTimeout(100000);
@@ -120,4 +112,3 @@ describe("Claim tests", claimsTests);
 describe("Utils tests", utilsTests);
 describe("Assets tests", assetsTests);
 describe("Staking tests", stakingTests);
-// describe("Identity creation tests", identityTests)
