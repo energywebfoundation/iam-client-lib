4201fcb491d21f0bdc242027dd56cd53ffada4e3
Jakub Sydor <jakub.sydor@gorrion.io>
2021-10-08 09:55:57 +0200

    fix(WalletConnect): fix walletconnect login
    The reason behind using `patch-package`: https://github.com/WalletConnect/walletconnect-utils/issues/3

--- src/iam/iam-base.ts
+++ src/iam/iam-base.ts
@@ -343,17 +343,17 @@ export class IAMBase {
             case "accountChanged": {
                 isMetamask
                     ? this._metamaskProvider?.on("accountsChanged", eventHandler)
-                    : this._walletConnectService.getProvider().wc.on("session_update", eventHandler);
+                    : this._walletConnectService.getProvider().on("session_update", eventHandler);
                 break;
             }
             case "disconnected": {
-                isMetamask === false && this._walletConnectService.getProvider()?.wc.on("disconnect", eventHandler);
+                isMetamask === false && this._walletConnectService.getProvider()?.on("disconnect", eventHandler);
                 break;
             }
             case "networkChanged": {
                 isMetamask
                     ? this._metamaskProvider?.on("networkChanged", eventHandler)
-                    : this._walletConnectService.getProvider().wc.on("session_update", eventHandler);
+                    : this._walletConnectService.getProvider().on("session_update", eventHandler);
                 break;
             }
             default:
