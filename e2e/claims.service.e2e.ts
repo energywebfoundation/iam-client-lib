ff37c536dfe99f2879c61777d956ea3390350b2d
JGiter <dimaadipmi@gmail.com>
2021-12-03 15:30:51 +0200

    test(claims): publishing onchain

--- e2e/claims.service.e2e.ts
+++ e2e/claims.service.e2e.ts
@@ -215,9 +215,7 @@ describe("Enrollment claim tests", () => {
         const assetDID = `did:${Methods.Erc1056}:${assetAddress}`;
         await enrolAndIssue(rootOwner, staticIssuer, { subjectDID: assetDID, claimType: `${roleName1}.${root}` });
 
-        return expect(
-            claimManager.hasRole(addressOf(assetDID), namehash(`${roleName1}.${root}`), version),
-        ).resolves.toBe(true);
+        return expect(claimsService.hasOnChainRole(assetDID, `${roleName1}.${root}`, version)).resolves.toBe(true);
     });
 
     test("enrollment by issuer of type ROLE", async () => {
@@ -238,9 +236,7 @@ describe("Enrollment claim tests", () => {
 
         await enrolAndIssue(rootOwner, dynamicIssuer, { subjectDID: rootOwnerDID, claimType: `${roleName2}.${root}` });
 
-        return expect(
-            claimManager.hasRole(addressOf(rootOwnerDID), namehash(`${roleName2}.${root}`), version),
-        ).resolves.toBe(true);
+        return expect(claimsService.hasOnChainRole(rootOwnerDID, `${roleName2}.${root}`, version)).resolves.toBe(true);
     });
 
     test("should enrol when prerequisites are met", async () => {
@@ -261,9 +257,7 @@ describe("Enrollment claim tests", () => {
         mockGetClaimsBySubject.mockImplementationOnce(() => [role1Claim]); // to verify issuance
 
         await enrolAndIssue(rootOwner, staticIssuer, { subjectDID: rootOwnerDID, claimType: `${roleName3}.${root}` });
-        return expect(
-            claimManager.hasRole(addressOf(rootOwnerDID), namehash(`${roleName3}.${root}`), version),
-        ).resolves.toBe(true);
+        return expect(claimsService.hasOnChainRole(rootOwnerDID, `${roleName3}.${root}`, version)).resolves.toBe(true);
     });
 
     test("should reject to enrol when prerequisites are not met", async () => {
@@ -286,22 +280,37 @@ describe("Enrollment claim tests", () => {
         ).rejects.toEqual(new Error(ERROR_MESSAGES.ROLE_PREREQUISITES_NOT_MET));
     });
 
-    test("should not enroll on-chain when requested registration off-chain", async () => {
-        const role4Claim = {
-            claimType: `${roleName4}.${root}`,
+    describe("Pubishing onchain", () => {
+        const registrationTypes = [RegistrationTypes.OnChain];
+        const claimType = `${roleName1}.${root}`;
+        const role1Claim = {
+            claimType,
             isAccepted: true,
         };
-        mockGetClaimsBySubject.mockImplementationOnce(() => [role4Claim]); // to verify requesting
 
-        await enrolAndIssue(rootOwner, staticIssuer, {
-            subjectDID: rootOwnerDID,
-            claimType: `${roleName4}.${root}`,
-            registrationTypes: [RegistrationTypes.OffChain],
+        test("should be able to issue and publish onchain", async () => {
+            mockGetClaimsBySubject.mockImplementationOnce(() => [role1Claim]); // to verify requesting
+
+            await enrolAndIssue(rootOwner, staticIssuer, {
+                subjectDID: rootOwnerDID,
+                claimType,
+                registrationTypes,
+                publishOnChain: true,
+            });
+            expect(await claimsService.hasOnChainRole(rootOwnerDID, claimType, version)).toBe(true);
         });
 
-        const hasRole = await claimManager.hasRole(addressOf(rootOwnerDID), namehash(`${roleName4}.${root}`), version);
+        test("should be able to issue without publishing onchain", async () => {
+            mockGetClaimsBySubject.mockImplementationOnce(() => [role1Claim]);
 
-        expect(hasRole).toBe(false);
+            await enrolAndIssue(rootOwner, staticIssuer, {
+                subjectDID: rootOwnerDID,
+                claimType,
+                registrationTypes,
+                publishOnChain: false,
+            });
+            expect(await claimsService.hasOnChainRole(rootOwnerDID, claimType, version)).toBe(false);
+        });
     });
 
     test("should issue claim request with additional issuer fields", async () => {
