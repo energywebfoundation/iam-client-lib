43c00be8973164255a2fd3d4c0afe8b57660de48
John Henderson <jrhender@users.noreply.github.com>
2021-12-10 12:20:46 -0700

    Revert "bug/ICS-124 random unauthorized to cache server "

--- src/modules/cacheClient/cacheClient.service.ts
+++ src/modules/cacheClient/cacheClient.service.ts
@@ -56,20 +56,21 @@ export class CacheClient implements ICacheClient {
             }
             if (await this.isAuthenticated()) {
                 this.refresh_token = refreshToken;
+                return;
             }
-        } catch {
-            const pubKeyAndIdentityToken = await this._signerService.publicKeyAndIdentityToken();
-            const {
-                data: { refreshToken, token },
-            } = await this.httpClient.post<{ token: string; refreshToken: string }>("/login", {
-                identityToken: pubKeyAndIdentityToken.identityToken,
-            });
-            if (!this.isBrowser) {
-                this.httpClient.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-            }
-            this.refresh_token = refreshToken;
-            this.pubKeyAndIdentityToken = pubKeyAndIdentityToken;
+        } catch {}
+
+        const pubKeyAndIdentityToken = await this._signerService.publicKeyAndIdentityToken();
+        const {
+            data: { refreshToken, token },
+        } = await this.httpClient.post<{ token: string; refreshToken: string }>("/login", {
+            identityToken: pubKeyAndIdentityToken.identityToken,
+        });
+        if (!this.isBrowser) {
+            this.httpClient.defaults.headers.common["Authorization"] = `Bearer ${token}`;
         }
+        this.refresh_token = refreshToken;
+        this.pubKeyAndIdentityToken = pubKeyAndIdentityToken;
 
         this.failedRequests = this.failedRequests.filter((callback) => callback());
     }
