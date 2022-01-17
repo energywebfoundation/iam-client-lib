f219a5e32b7dc6174fa6e87726d74c2189b01f34
JGiter <dimaadipmi@gmail.com>
2022-01-17 17:51:30 +0200

    fix(didRegistry): update asset document

---
+++ e2e/cacheClient.e2e.ts
@@ -0,0 +1,36 @@
+import { initWithPrivateKeySigner, setCacheConfig } from "../src";
+
+const rpcUrl = "https://volta-rpc.energyweb.org";
+const privateKey = "";
+
+describe.skip("Cache client", () => {
+    test("Asset owner should be able to update asset document", async () => {
+        setCacheConfig(73799, {
+            url: "https://identitycache-dev.energyweb.org/v1",
+            cacheServerSupportsAuth: true,
+        });
+
+        const metadata = { meterId: "SolarM" };
+
+        const { connectToCacheServer } = await initWithPrivateKeySigner(privateKey, rpcUrl);
+        const { connectToDidRegistry, assetsService } = await connectToCacheServer();
+        const { claimsService, didRegistry } = await connectToDidRegistry();
+
+        const assetAddress = await assetsService.registerAsset();
+        const assetDid = `did:ethr:volta:${assetAddress}`;
+
+        const claimUrl = await claimsService.createSelfSignedClaim({
+            data: metadata,
+            subject: assetDid,
+        });
+
+        await new Promise((r) => setTimeout(r, 30_000));
+
+        const assetDoc = await didRegistry.getDidDocument({ did: assetDid, includeClaims: true });
+        expect(
+            assetDoc.service.some(
+                (s) => s.serviceEndpoint === claimUrl && expect(s).toEqual(expect.objectContaining(metadata)),
+            ),
+        );
+    }, 120000);
+});
