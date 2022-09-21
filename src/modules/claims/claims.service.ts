ba6f7ba5bf9f8abe7ae0ba20bd227ab43a727ac5
whitneypurdum <whitney.purdum@energyweb.org>
2022-09-21 11:21:22 -0400

    docs: add asset credential registration to documentation

--- src/modules/claims/claims.service.ts
+++ src/modules/claims/claims.service.ts
@@ -504,7 +504,7 @@ export class ClaimsService {
   }
 
   /**
-   * Register issued on-chain claim on Claim Manager contract.
+   * Register issued on-chain claim on Claim Manager contract Can be used by asset owners to register credentials on-chain for their assets.
    *
    * ```typescript
    * const claim: Claim = await claimsService.getClaimById('7281a130-e2b1-430d-8c14-201010eae901');
