256750155f4e692292e107263dc2746be4740d7d
JGiter <dimaadipmi@gmail.com>
2023-07-28 12:26:13 +0300

    feat: add winston logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,19 @@
+import winston, { format } from 'winston';
+
+const { combine, colorize, simple } = format;
+
 export enum LogLevel {
-  debug = 1,
-  info = 2,
-  warn = 3,
-  error = 4,
+  error = 'error',
+  warn = 'warn',
+  info = 'info',
+  debug = 'debug',
 }
 
 /*
  * Abstract overridable class for logger
  */
 export abstract class ILogger {
-  constructor(protected readonly _logLevel: LogLevel = LogLevel.debug) {}
+  constructor(protected readonly maxLevel: LogLevel = LogLevel.debug) { }
 
   debug = (message: unknown) => this.log(message, LogLevel.debug);
   info = (message: unknown) => this.log(message, LogLevel.info);
@@ -17,7 +21,7 @@ export abstract class ILogger {
   error = (message: unknown) => this.log(message, LogLevel.error);
 
   log = (message: unknown, level: LogLevel) =>
-    this._logLevel <= level && this._log(message, level);
+    this._log(message, level);
 
   protected abstract _log(message: unknown, level: LogLevel): void;
 }
@@ -26,21 +30,17 @@ export abstract class ILogger {
  * Default Implementation of ILogger streaming logs to console
  */
 export class ConsoleLogger extends ILogger {
-  _log = (message: unknown, level: LogLevel) => {
-    switch (level) {
-      case LogLevel.debug:
-        console.debug(message);
-        break;
-      case LogLevel.info:
-        console.log(message);
-        break;
-      case LogLevel.warn:
-        console.warn(message);
-        break;
-      case LogLevel.error:
-      default:
-        console.error(message);
-        break;
-    }
+  private logger = winston.createLogger({
+    level: this.maxLevel,
+    format: combine(
+      colorize(),
+      simple()
+    ),
+    transports:
+      new winston.transports.Console(),
+  });
+
+  _log(message: unknown, level: LogLevel) {
+    this.logger[level](message);
   };
 }
